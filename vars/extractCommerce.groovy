5ad91a96ead0bb4c3fac066f0802ed8d3a0c1a58
gauravgoyal703 <4189916+gauravgoyal703@users.noreply.github.com>
2021-05-24 16:25:42 +1200

    Update extractCommerce.groovy
    Changing it to 2011 from 2005

--- vars/extractCommerce.groovy
+++ vars/extractCommerce.groovy
@@ -1,8 +1,8 @@
 def call(commerceDir) {
     echo "##### Extract commerce platform ##### -- ${commerceDir}"
-    sh "unzip -o ../CXCOM2005*.ZIP -d ${commerceDir}/core-customize"
+    sh "unzip -o ../CXCOMM2011*.ZIP -d ${commerceDir}/core-customize"
     /** Uncomment if you will be using the Integration Extension Pack
     echo "##### Extract commerce integration pack #####"
-    sh "unzip -o ../CXCOMINTPK2005*.ZIP -d ${commerceDir}/core-customize"
+    sh "unzip -o ../CXCOMINTPK2011*.ZIP -d ${commerceDir}/core-customize"
     **/
 }  
