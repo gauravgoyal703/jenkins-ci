d5e4cebc22ca07e6c2d5d82f3b18f9323fa61ed5
gauravgoyal703 <4189916+gauravgoyal703@users.noreply.github.com>
2021-05-25 15:11:36 +1200

    Update buildCommerceCloud.groovy

--- vars/buildCommerceCloud.groovy
+++ vars/buildCommerceCloud.groovy
@@ -2,9 +2,9 @@ def call(branch, buildName) {
     echo "##### Initiate Build to SAP Commerce Cloud Environment #####"
     //deploy tag 
     script{
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'SUBSCRIPTIONID', passwordVariable: 'TOKEN']]) {
-            portalURL = 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/$SUBSCRIPTIONID/builds';
-            authorizationValue = 'Bearer $TOKEN'
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
+            portalURL = 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/$subscriptionId/builds';
+            authorizationValue = 'Bearer $token'
             echo "$portalURL"
             echo "$authorizationValue"
             build = sh (script: "curl --location --request POST ${portalURL} --header 'Content-Type: application/json' --header 'Authorization: ${authorizationValue}' --header 'Content-Type: text/plain' --data-raw '{\"branch\": \"${branch}\",\"name\": \"${buildName}\"}'",returnStdout:true)
