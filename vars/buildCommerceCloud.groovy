fd9f9af5fb7b236c30abe4adb3f1e1ee0a47f33f
gauravgoyal703 <4189916+gauravgoyal703@users.noreply.github.com>
2021-05-25 15:12:36 +1200

    Update buildCommerceCloud.groovy

--- vars/buildCommerceCloud.groovy
+++ vars/buildCommerceCloud.groovy
@@ -3,10 +3,8 @@ def call(branch, buildName) {
     //deploy tag 
     script{
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
-            portalURL = 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/$subscriptionId/builds';
-            authorizationValue = 'Bearer $token'
-            echo "$portalURL"
-            echo "$authorizationValue"
+            portalURL = "https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/builds";
+            authorizationValue = "Bearer ${token}"
             build = sh (script: "curl --location --request POST ${portalURL} --header 'Content-Type: application/json' --header 'Authorization: ${authorizationValue}' --header 'Content-Type: text/plain' --data-raw '{\"branch\": \"${branch}\",\"name\": \"${buildName}\"}'",returnStdout:true)
             echo "$build"
             build_result = readJSON text: "$build"
